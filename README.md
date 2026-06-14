6ed25f43edf739864cf6e7068f738ace03c9af9a
CookieCarter <cookiecarter362010@gmail.com>
2026-06-14 16:10:07 -0600

    Move log function into a seperate function. Search area is now centered on the player. Add optional parameter to specify log radius. Response messsage now says how many blocks were logged. Made file in response message clickable.
    Updated README.
    Updated mod description.

--- README.md
+++ README.md
@@ -1,5 +1,15 @@
 ## Chunk Logger
 
-A simple mod which will log bedrock in the nether to a file. Meant to be used in conjunction with a [seed cracker](https://github.com/19MisterX98/Nether_Bedrock_Cracker).
+A simple mod which will log bedrock (on y-levels 4 and 123) in the Nether to a file. Meant to be used in conjunction with a [bedrock seed cracker](https://github.com/19MisterX98/Nether_Bedrock_Cracker). (Said seed cracker only supports importing up to 200 blocks.)
 
-To use just type '/logbedrock' while in the nether.
+### Usage
+
+To use just type '/logbedrock' while in the Nether.
+
+You can also specify a radius as an optional parameter (e.g. '/logbedrock 10'). By default the command has a radius of 8 blocks.
+> [!WARNING]
+> Entering large values will cause the game to hang.
+
+For the best results with the seed cracker, input a radius that gives ~200 blocks.
+
+The file will be saved in the root directory of your minecraft instance.
--- src/client/java/cookie/chunklogger/client/ChunkLoggerClient.java
+++ src/client/java/cookie/chunklogger/client/ChunkLoggerClient.java
@@ -2,11 +2,15 @@ package cookie.chunklogger.client;
 
 import net.fabricmc.api.ClientModInitializer;
 
+import com.mojang.brigadier.arguments.IntegerArgumentType;
+import com.mojang.brigadier.context.CommandContext;
 import net.fabricmc.fabric.api.client.command.v2.ClientCommands;
 import net.fabricmc.fabric.api.client.command.v2.ClientCommandRegistrationCallback;
 import net.fabricmc.fabric.api.client.command.v2.FabricClientCommandSource;
 import net.minecraft.core.BlockPos;
 import net.minecraft.network.chat.Component;
+import net.minecraft.ChatFormatting;
+import net.minecraft.network.chat.ClickEvent.OpenFile;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.phys.Vec3;
 import net.minecraft.client.multiplayer.ClientLevel;
@@ -22,60 +26,76 @@ public class ChunkLoggerClient implements ClientModInitializer {
     public static final String MOD_ID = "chunklogger";
     public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
+    private static int logBedrock(CommandContext<FabricClientCommandSource> context, int radius) {
+        // Get the source (player) of the command.
+        FabricClientCommandSource source = context.getSource();
+        ClientLevel level = source.getLevel();
+
+        // Check if player isn't in the Nether.
+        if (level.dimension() != Level.NETHER) {
+            source.sendError(Component.literal("Command can only be used in the nether."));
+            return 0;
+        }
+
+        Vec3 playerPos = source.getPosition();
+
+        int blocks = 0;
+
+        final String filename = "bedrock.txt";
+
+        try (FileOutputStream output = new FileOutputStream(filename)) {
+
+            // Check y-level 4 and 123.
+            for (int y = 4; y <= 123; y += 119) {
+                // Check 'radius' * 2 blocks in the x and z axes around the player.
+                for (int x = (int) playerPos.x - radius; x < (int) playerPos.x + radius; x++) {
+                    for (int z = (int) playerPos.z - radius; z < (int) playerPos.z + radius; z++) {
+                        BlockPos pos = new BlockPos(x, y, z);
+                        
+                        // Get the block type.
+                        BlockState state = level.getBlockState(pos);
+                        String blockId = state.getBlock().toString();
+
+                        // Write block to file if it is bedrock.
+                        if (blockId.equals("Block{minecraft:bedrock}")) {
+                            String text = "%d %d %d Bedrock\n".formatted(x, y, z);
+                            output.write(text.getBytes());
+                            blocks++;
+                        }
+                    }
+                }
+            }
+            
+            output.close();
+        } catch (IOException e) {
+            LOGGER.error("Failed to write to file!");
+            source.sendError(Component.literal("Failed to write to file!"));
+            return 0;
+        }
+
+
+        source.sendFeedback(Component.literal("Logged %d blocks to ".formatted(blocks))
+            .append(Component.literal(filename)
+            .withStyle(ChatFormatting.UNDERLINE)
+            .withStyle(s -> s.withClickEvent(new OpenFile(filename)))));
+        
+        return 1; // Command succeeded
+    }
+
     @Override
     public void onInitializeClient() {
         // Register the client-side command callback
         ClientCommandRegistrationCallback.EVENT.register((dispatcher, registryAccess) -> {
             dispatcher.register(ClientCommands.literal("logbedrock")
                 .executes(context -> {
-                    // Get the source (player) of the command.
-                    FabricClientCommandSource source = context.getSource();
-                    ClientLevel level = source.getLevel();
-
-                    if (level.dimension() != Level.NETHER) {
-                        source.sendError(Component.literal("Command can only be used in the nether."));
-                        return 0;
-                    }
-
-                    Vec3 playerPos = source.getPosition();
-
-                    final int dist = 16;
-
-                    try (FileOutputStream output = new FileOutputStream("bedrock.txt")) {
-
-                        // Check y-level 4 and 123.
-                        for (int y = 4; y <= 123; y += 119) {
-                            // Check up to 16 blocks in the x and z axes.
-                            for (int x = (int) playerPos.x; x < (int) playerPos.x + dist; x++) {
-                                for (int z = (int) playerPos.z; z < (int) playerPos.z + dist; z++) {
-                                    BlockPos pos = new BlockPos(x, y, z);
-                                    
-                                    // Get the block type.
-                                    BlockState state = level.getBlockState(pos);
-                                    String blockId = state.getBlock().toString();
-
-                                    // Write block to file if it is bedrock.
-                                    if (blockId.equals("Block{minecraft:bedrock}")) {
-                                        String text = "%d %d %d Bedrock\n".formatted(x, y, z);
-                                        output.write(text.getBytes());
-                                        // LOGGER.info("Bedrock at [" + x + ", " + y + ", " + z + "]");
-                                    }
-                                }
-                            }
-                        }
-                        
-                        output.close();
-                    } catch (IOException e) {
-                        LOGGER.error("Failed to write to file!");
-                        source.sendError(Component.literal("Failed to write to file!"));
-                        return 0;
-                    }
-
-                    source.sendFeedback(Component.literal("Blocks logged to bedrock.txt."));
-                    
-                    return 1; // Command succeeded
+                    // Default to a radius of 8.
+                    return logBedrock(context, 8);
                 })
-            );
+                .then(ClientCommands.argument("radius", IntegerArgumentType.integer())
+                    .executes(context -> {
+                        int distance = IntegerArgumentType.getInteger(context, "radius");
+                        return logBedrock(context, distance);
+                    })));
         });
     }
 }
